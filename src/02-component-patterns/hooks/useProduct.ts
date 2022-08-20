5c7aba8c2140bb0d6efda99c520b7ec6e7ab6df1
jonathanh21 <jhonathan.herrerao@gmail.com>
2022-08-20 11:45:11 -0500

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductArgs {
