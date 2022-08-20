ae361bca97d5ee475d41552b6b81379d382a9586
jonathanh21 <jhonathan.herrerao@gmail.com>
2022-08-20 11:43:03 -0500

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,29 +8,14 @@ export const useShoppingCart = () => {
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
     setShoppingCart((prev) => {
-      const productInCart: ProductInCart = prev[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...prev,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = prev;
+        return rest;
       }
-
-      const { [product.id]: toDelete, ...rest } = prev;
-      return rest;
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = prev;
-      //   return rest;
-      // }
-      // return {
-      //   ...prev,
-      //   [product.id]: { ...product, count },
-      // };
+      return {
+        ...prev,
+        [product.id]: { ...product, count },
+      };
     });
   };
   return { shoppingCart, onProductCountChange };
